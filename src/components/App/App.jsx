a53ae7235d806b4bee15c85d218d02cc72341c4d
onisimsuldac <onnisimsuldac@gmail.com>
2024-03-26 14:02:51 +0200

    commit fin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,6 +9,7 @@ const Cast = lazy(() => import('components/Cast/Cast'));
 const Reviews = lazy(() => import('components/Reviews/Reviews'));
 // const NotFound = lazy(() => import('pages/NotFound/NotFound'));
 
+
 const App = () => {
   return (
     <Routes>
